1b40225ab954716845a5475b6628d5be4d5e6e77
darwin-dh <darwinquishpe93@gmail.com>
2022-11-21 23:43:04 -0500

    DQ_CRUD_clients

--- src/router/routes.js
+++ src/router/routes.js
@@ -192,4 +192,32 @@ export default [
     },
     component: () => import("@/views/Accounting/Plan/Plan.vue")
   },
+  //sales
+  {
+    path: "/Clientes",
+    name: "Clientes",
+    meta: {
+      title: "Clientes"
+
+    },
+    component: () => import("@/views/Sales/Clients/Clients.vue")
+  },
+  {
+    path: "/AgregarClientes",
+    name: "Agregar Clientes",
+    meta: {
+      title: "Agregar Clientes"
+
+    },
+    component: () => import("@/views/Sales/Clients/Crud/AddClients.vue")
+  },
+  {
+    path: "/EditarClientes/:id",
+    name: "EditarClientes",
+    meta: {
+      title: "Editar Clientes"
+
+    },
+    component: () => import("@/views/Sales/Clients/Crud/EditClients.vue")
+  },
 ];
